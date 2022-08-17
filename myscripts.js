c19a372974e25895d9646218ef3aaf09ce6baeed
Jake Jepson <jwjepson1@gmail.com>
2022-08-17 00:07:29 -0400

    prompt user for size change

--- myscripts.js
+++ myscripts.js
@@ -4,12 +4,17 @@ container.classList.add("grid-container");
 
 body.appendChild(container);
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function changeSize() {
+    let size = prompt("What size would you like?");
+    createGrid(size);
+}
+
+function createGrid(size = 16) {
+    for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const column = document.createElement("div");
             column.classList.add("column");
             row.appendChild(column);
@@ -23,3 +28,6 @@ squares = document.querySelectorAll(".column");
 squares.forEach(square => square.addEventListener("mouseover", () => {
     square.style.backgroundColor = "black";
 }));
+
+sizeButton = document.querySelector(".change-size");
+sizeButton.addEventListener("click", changeSize);
