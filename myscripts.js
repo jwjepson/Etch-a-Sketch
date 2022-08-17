92bdeb19c72b221c8f43b8a19ad835379c8c77a5
Jake Jepson <jwjepson1@gmail.com>
2022-08-16 23:09:37 -0400

    create createGrid function

---
+++ myscripts.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+const container = document.createElement("div");
+container.classList.add("grid-container");
+
+body.appendChild(container);
+
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < 16; j++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+    }
+}
+
+createGrid();
